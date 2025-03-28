310997dd411f1ca8753ab70a9fb7febaf0935ade
Leticia Menezes <dev.leticiamenezes@gmail.com>
2025-03-28 18:04:53 -0300

    Corrigindo formatação com Prettier

--- cypress/e2e/Login.spec.cy.ts
+++ cypress/e2e/Login.spec.cy.ts
@@ -1,20 +1,28 @@
-describe('Login com sucesso', () => {
-  it('Deve fazer login com um usuário válido', () => {
-   cy.visit('http://localhost:3000/')
-   cy.get('#username').type('Dina20')
-   cy.get('#password').type('s3cret')
-   cy.get('[data-test="signin-submit"]').click()
-   cy.get('[data-test="main"]').should('be.visible')
-   cy.get('[data-test="sidenav-signout"]').should('be.visible')
+describe("Login com sucesso", () => {
+  const selectorsList = {
+    baseUrl: "http://localhost:3000/",
+    usernameField: "",
+    passwordField: "",
+    submit: "",
+    confirmLogin: "",
+  };
+
+  it("Deve fazer login com um usuário válido", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#username").type("Dina20");
+    cy.get("#password").type("s3cret");
+    cy.get('[data-test="signin-submit"]').click();
+    cy.get('[data-test="main"]').should("be.visible");
+    cy.get('[data-test="sidenav-signout"]').should("be.visible");
   });
 });
 
-describe('Tentar fazer login com credenciais inválidas', () => {
-  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('#username').type('usuarioteste')
-    cy.get('#password').type('senhateste')
-    cy.get('[data-test="signin-submit"]').click()
-    cy.get('[data-test="signin-error"]').should('be.visible')
+describe("Tentar fazer login com credenciais inválidas", () => {
+  it("Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#username").type("usuarioteste");
+    cy.get("#password").type("senhateste");
+    cy.get('[data-test="signin-submit"]').click();
+    cy.get('[data-test="signin-error"]').should("be.visible");
   });
 });
