bbab87c9e7f9976670ad540e3bd9db6b0c2bad19
Leticia Menezes <dev.leticiamenezes@gmail.com>
2025-03-28 16:20:08 -0300

    First commit

---
+++ cypress/e2e/Login.spec.cy.ts
@@ -0,0 +1,20 @@
+describe('Login com sucesso', () => {
+  it('Deve fazer login com um usuário válido', () => {
+   cy.visit('http://localhost:3000/')
+   cy.get('#username').type('Dina20')
+   cy.get('#password').type('s3cret')
+   cy.get('[data-test="signin-submit"]').click()
+   cy.get('[data-test="main"]').should('be.visible')
+   cy.get('[data-test="sidenav-signout"]').should('be.visible')
+  });
+});
+
+describe('Tentar fazer login com credenciais inválidas', () => {
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#username').type('usuarioteste')
+    cy.get('#password').type('senhateste')
+    cy.get('[data-test="signin-submit"]').click()
+    cy.get('[data-test="signin-error"]').should('be.visible')
+  });
+});
---
+++ cypress/e2e/Registro.spec.cy.ts
@@ -0,0 +1,27 @@
+describe('Registro de novo usuário com sucesso', () => {
+    it.only('Deve registrar um novo usuário com informações válidas', () => {
+      cy.visit('http://localhost:3000/')
+      cy.get('[data-test="signup"]').click()
+      cy.get('#firstName').type('Nometeste')
+      cy.get('#lastName').type('Sobrenometeste')
+      cy.get('#username').type('userteste')
+      cy.get('#password').type('s3nha')
+      cy.get('#confirmPassword').type('s3nha')
+      cy.get('[data-test="signup-submit"]').click()
+      cy.get('.SignInForm-paper').should('be.visible').and('contain', 'Sign In')      
+    });
+  });
+
+  describe('Tentar registrar um novo usuário com informações incompletas', () => {
+    it('Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias', () => {
+      cy.visit('http://localhost:3000/')
+      cy.get('[data-test="signup"]').click()
+      cy.get('#firstName').type('Nometeste')
+      cy.get('#lastName').type('Sobrenometeste')
+      cy.get('#username').type('userteste')
+      cy.get('#password').type('s3nha1')
+      cy.get('#confirmPassword').type('s3nha2')
+      cy.get('[data-test="signup-submit"]').click({ force: true })
+      cy.get('.MuiTypography-h5').should('be.visible').and('contain', 'Sign Up')
+    });
+  });
